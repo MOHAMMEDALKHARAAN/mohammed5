55d76520ad60df01a8c2bc1aedccdfd30ddb3bdf
MOHAMMEDALKHARAAN <alkharaanm@hotmail.com>
2025-11-08 19:57:23 +0300

    قبل mvt

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # روابط تطبيق core ستُضاف هنا لاحقًا
+    path('', views.home, name='home'),  # الصفحة الرئيسية
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """عرض الصفحة الرئيسية للموقع"""
+    return render(request, 'home.html')
